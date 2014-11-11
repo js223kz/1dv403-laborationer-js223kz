21f185ea0b3280e864abd4144d9694d5b87f6d5e
Johanna Szepanski <js223kz@student.lnu.se>
2014-11-11 12:07:14 +0000

    sorted names in array

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,16 @@
 
 var makePerson = function(persArr){
 
-
-	// Din kod här...
+    var arrayData = [];
+    arrayData.names = arrayData.push(persArr[0].name, persArr[1].name, persArr[2].name);
+    arrayData.sort(arrayData.names);
+    console.log(arrayData);
+    
+    
+    
+    //var max_of_array = Math.max.apply(Math, array);
+    
+    return arrayData;
 
 }
 
