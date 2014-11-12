fd24af6c3c4bbf882c6fbfab0760d89d1d4323c4
Johanna Szepanski <js223kz@student.lnu.se>
2014-11-12 15:35:46 +0000

    had an brilliant idea just trying it out

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,36 +1,41 @@
 "use strict";
 
 var makePerson = function(persArr){
-    var i = 0;
+    
+    function logArrayElements(elem, index, arr) {
+  console.log(elem.name);
+}
+
+// Note ellision, there is no member at 2 so it isn't visited
+        persArr.forEach(logArrayElements);
+            
+    
+    
+    /*var i = 0;
     var namesArray = [];
     var agesArray = [];
     var average = 0;
-    var hello = 'å'.localeCompare('a');
-    var list = "å ä ö".split(" ");
 
-console.log(list);
-    console.log(hello);
     for(i=0; i<persArr.length;i++){
         namesArray[i] = persArr[i].name;
         agesArray[i] = persArr[i].age;
         average += persArr[i].age;
     }
-    //namesArray.sort();
     namesArray.sort(function(a, b) {
-  return a.localeCompare(b);
+        return a.localeCompare(b);
 });
    var newString = namesArray.join(", ");
    
    agesArray.sort();
    var minAge = agesArray[0];
    var maxAge = agesArray[agesArray.length -1];
-   average = Math.round(average / agesArray.length);
+   average = Math.round(average / agesArray.length);*/
     var result = {};
-    result.minAge = minAge;
+    /*result.minAge = minAge;
     result.maxAge = maxAge;
     result.averageAge = average;
     result.names = newString;
-    console.log(result);
+    console.log(result);*/
     return result;
 }
 
