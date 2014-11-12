78646b2073011bb789d52fe86733e684b2e4eef8
Johanna Szepanski <js223kz@student.lnu.se>
2014-11-12 16:25:06 +0000

    all tests green in my attempt to go for a goldstar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -5,50 +5,42 @@ var makePerson = function(persArr){
     var agesArray = [];
     var nameString;
     var totalAge;
+    var averageAge;
+    var minAge;
+    var maxAge;
     
     function logArrayElements(elem, index, arr) {
             namesArray[index] = elem.name;
-            agesArray[index] = elem.age;
+            agesArray[index]= +elem.age;
     }
+   
 
     persArr.forEach(logArrayElements);
-            
     namesArray.sort(function(a, b) {
         return a.localeCompare(b);
     });
         
     nameString= namesArray.join(", ");
     
-    agesArray.reduce(function(sum, elem, index, arr){
-    totalAge = sum + elem;
+    agesArray.sort();
+    
+    minAge = agesArray[0];
+    maxAge = agesArray[agesArray.length -1];
+
+    totalAge = agesArray.reduce(function(sum, elem, index, arr){
+        return sum + elem;
     });
-
     
-    /*var i = 0;
-    
-    var agesArray = [];
-    var average = 0;
+    averageAge = Math.round(totalAge / agesArray.length);
 
-    for(i=0; i<persArr.length;i++){
-        namesArray[i] = persArr[i].name;
-        agesArray[i] = persArr[i].age;
-        average += persArr[i].age;
-    }
-
-   
-   agesArray.sort();
-   var minAge = agesArray[0];
-   var maxAge = agesArray[agesArray.length -1];
-   average = Math.round(average / agesArray.length);*/
     var result = {};
+    result.minAge = minAge;
+    result.maxAge = maxAge;
+    result.averageAge = averageAge;
     result.names = nameString;
     
-    console.log(nameString);
-    /*result.minAge = minAge;
-    result.maxAge = maxAge;
-    result.averageAge = average;
+    console.log(result);
     
-    console.log(result);*/
     return result;
 }
 
