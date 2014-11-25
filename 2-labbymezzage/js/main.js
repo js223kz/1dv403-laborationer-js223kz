de5942dbc5fff2acc9b345ae5127c1a602fc8a26
Johanna Szepanski <js223kz@student.lnu.se>
2014-11-25 16:15:39 +0000

    playing with less

--- 2-labbymezzage/js/main.js
+++ 2-labbymezzage/js/main.js
@@ -4,11 +4,10 @@ var MyMessages = {
     messages: [],
     
     run : function(){
-        var mess1 = new Message("Meddelande1", new Date());
+        messages.push(new Message("Meddelande1", new Date())) ;
         var mess2 = new Message("Meddelande2", new Date());
         var mess3 = new Message("Meddelande3", new Date());
         
-        messages.push(mess1, mess2, mess3);
 
         console.log(MyMessages.messages[0].getDateText());
     }
