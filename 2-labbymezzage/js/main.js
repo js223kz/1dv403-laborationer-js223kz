e5bb0ac24a63bc572c971e10de0e5cc07c799a25
Johanna Szepanski <js223kz@student.lnu.se>
2014-11-28 10:46:08 +0000

    deletebutton

--- 2-labbymezzage/js/main.js
+++ 2-labbymezzage/js/main.js
@@ -1,64 +1,86 @@
 "use strict";
 var MyMessages = {
     messages: [], 
+    index: 0,
 
    run : function(){
         var submitButton = document.querySelector("button");
         var txtArea = document.querySelector("textarea");
         var numberOfMessages = document.getElementById("numberofmessages");
         numberOfMessages.innerHTML = 'Antal meddelanden: 0';
+        var newMessage;
        
+        
+        
+        
+        
         txtArea.onkeydown = function(e){
             if(e.keyCode === 13 && !e.shiftKey){
                 createNewMessage();
-                addMessageToList();
                 txtArea.blur();
             }
         };
         
         submitButton.addEventListener("click", function(e){
             createNewMessage();
-            
-             
         });
+        
+         
+        
         
         function createNewMessage(){
             var chatMessage = document.querySelector("textarea");
-            var newMessage = new Message(chatMessage.value, new Date());
+            newMessage = new Message(chatMessage.value, new Date());
             MyMessages.messages.push(newMessage);
-            numberOfMessages.innerHTML = 'Antal meddelanden: ' + MyMessages.messages.length;
-            addMessageToList();
+            
+            
+            renderList();
+            
             //clears textarea after submitting message
             txtArea.value ="";
         }
         
-        function addMessageToList(){
+        function renderList(){
             var listOfMessages = document.getElementById("listofmessages");
-            
-            //clear list of messages
             listOfMessages.innerHTML = "";
-           
-           //loop through array to present each message in array
-            MyMessages.messages.forEach(function(entry) {
-                
+            showNumberOfMessages();
+            
+            MyMessages.messages.forEach(function(item){
+                var index = MyMessages.messages.indexOf(item);
                 
                 var listItem = document.createElement("li");
                 listOfMessages.appendChild(listItem); 
                 
-                var deleteBtn = document.createElement("button");
+                var deleteBtn = document.createElement("button");  
                 deleteBtn.id = "deletebutton";
-                listItem.appendChild(deleteBtn); 
+                listItem.appendChild(deleteBtn);
+                 
+                deleteBtn.addEventListener("click", function(e){
+                    removeMessagesFromList(index);
+                });  
                 
+                var timeStampBtn = document.createElement("button");
+                timeStampBtn.id = "timestampbutton";
+                listItem.appendChild(timeStampBtn);
+        
                 var chatText = document.createElement("p");
                 chatText.id = "chattext";
-                chatText.innerHTML = entry.getHTMLText();
+                chatText.innerHTML = item.getHTMLText();
                 listItem.appendChild(chatText);
-                
+                    
                 var timeStamp = document.createElement("p");
                 timeStamp.id = "timestamp";
-                timeStamp.innerHTML = entry.getDateText();
+                timeStamp.innerHTML = item.getDateText();
                 listItem.appendChild(timeStamp);
-            });
+                
+            });  
+        }
+        function showNumberOfMessages(){
+            numberOfMessages.innerHTML = 'Antal meddelanden: ' + MyMessages.messages.length;
+        }
+        function removeMessagesFromList(index){
+            MyMessages.messages.splice(index, 1);
+            renderList();
         }
     }
 };
