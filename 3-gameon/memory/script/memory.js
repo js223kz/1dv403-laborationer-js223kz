a519d1eeb14b3587604cea5dc76d53e0697d944c
Johanna Szepanski <js223kz@student.lnu.se>
2014-12-08 10:51:33 +0000

    pushed obect to array

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -6,7 +6,10 @@
         
         init : function(){
         var newBoard = new RandomGenerator.getPictureArray(3, 4);
+        
+        Memory.memoryArray.push(newBoard);
         console.log(newBoard); 
+        console.log(Memory.memoryArray);
         }
     }; 
 window.onload = Memory.init;
