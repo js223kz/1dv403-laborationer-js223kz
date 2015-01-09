e06d6a58150a92baae937b992082b166942eefe8
Johanna Szepanski <js223kz@student.lnu.se>
2015-01-09 08:17:12 +0000

    comments

--- 4-thequiz/main.js
+++ 4-thequiz/main.js
@@ -3,15 +3,14 @@
 var JOSZEP = JOSZEP || {};
 
 JOSZEP.init = function(){
-    var id = 1;
-    var url = "http://vhost3.lnu.se:20080/question/";
-      
-    getJson(url);
+    var url = "http://vhost3.lnu.se:20080/question/1";
     
+    getJson(url);
+    //connect to server and get json result
     function getJson(url){
         var response = null;
         var AJAX_req = new XMLHttpRequest();
-            AJAX_req.open("GET", url + id, true);
+            AJAX_req.open("GET", url, true);
         
         AJAX_req.onreadystatechange = function(){
             
@@ -22,30 +21,40 @@ JOSZEP.init = function(){
         };
         AJAX_req.send(null); 
     }
+    //present the question
     function getQuestion(response){
         var question = document.querySelector(".question");
-            question.innerHTML = response.question;
+        question.innerHTML = response.question;
+        
         var input = document.getElementById("textfield");
         var answerbutton = document.getElementById("answerbutton");
-                
-            answerbutton.addEventListener("click", function(e){
-                e.preventDefault();
-                sendAnswer(input.value, response);
-            });
-    }
-    function sendAnswer(inputvalue, response){
         
-        var answerResponse = null;
+        answerbutton.addEventListener("click", function(e){
+            e.preventDefault();
+            sendAnswer(input.value, response);
+        });
+    }
+    //send user answer to server and get result
+    function sendAnswer(inputvalue, response){
+        var answerResponse;
         var AJAX_post = new XMLHttpRequest();
+        
         AJAX_post.onreadystatechange = function() {
             
-            if(AJAX_post.readyState == 4 && AJAX_post.status == 200) {
-              answerResponse = JSON.parse(AJAX_post.responseText);
-              console.log(answerResponse.message);
+            if(AJAX_post.readyState == 4) {
+                answerResponse = JSON.parse(AJAX_post.responseText);
+                
+                if(answerResponse.message === "Correct answer!"){
+                    console.log(answerResponse.message);
+                    getJson(answerResponse.nextURL);
+                }else{
+                    console.log(answerResponse.message);
+                }
             }
         };
         AJAX_post.open("POST", response.nextURL, true);
         AJAX_post.setRequestHeader('Content-Type', 'application/json');
+        
         var answer = JSON.stringify({"answer":inputvalue});
         AJAX_post.send(answer);
     }
