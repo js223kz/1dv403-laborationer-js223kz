97d9226055be47471b895287f98c576ac054b8cf
Johanna Szepanski <js223kz@student.lnu.se>
2015-01-13 16:10:12 +0000

    done

--- 5-pwd/js/ThumbNailWindow.js
+++ 5-pwd/js/ThumbNailWindow.js
@@ -7,68 +7,54 @@ ThumbNailWindow.prototype = new TemplateWindow();
 
 ThumbNailWindow.prototype.renderContent = function (){
     var templateWindow = document.getElementById(this.id);
-    
-    var response = null;
+    var templateContent = templateWindow.querySelector(".templatecontent");
+    var imageWrapper = null;
     var jsonArray = [];
-
+    var maxWidth = 0;
+    var maxHeight = 0;
     var AJAX_req = new XMLHttpRequest();
         AJAX_req.open( "GET","https://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
         
         AJAX_req.onreadystatechange = function(){
             if( AJAX_req.readyState == 4 && AJAX_req.status == 200){
-                response = JSON.parse(AJAX_req.responseText);
-                renderGridLayout(response);
+                jsonArray = JSON.parse(AJAX_req.responseText);
+                
+                jsonArray.forEach(function(item){
+                     if(item.thumbWidth > maxWidth){
+                        maxWidth = item.thumbWidth;
+                    }
+                     if(item.thumbHeight > maxHeight){
+                        maxHeight = item.thumbWidth;
+                    }
+                })
+                jsonArray.forEach(function(item){
+                   
+                    imageWrapper = document.createElement("div");
+                    imageWrapper.setAttribute("class", "imagewrapper"); 
+                    imageWrapper.style.width = maxWidth + 5 + "px";
+                    imageWrapper.style.height = maxHeight + 5 + "px";   
+                
+                var thumbNailImage = document.createElement("img");
+                    thumbNailImage.setAttribute("class", "thumbnailimage");
+                    thumbNailImage.src = item.thumbURL;
+                     
+                    
+                    thumbNailImage.addEventListener("click", function(e){
+                        changeBackground(item.URL);
+                    }); 
+                    console.log(maxWidth);
+                    
+                    imageWrapper.appendChild(thumbNailImage);
+                    templateContent.appendChild(imageWrapper);
+                });
+                   
                 templateWindow.querySelector(".activityindicator").style.display = "none";
             }
         };
     AJAX_req.send(null); 
-    
-    function renderGridLayout(response){
-        
-        for(var item in response){
-            jsonArray.push({URL:response[item].URL, thumbURL:response[item].thumbURL, width:response[item].width, height: response[item].height});
-        }
 
-        var numberOfRows = Math.ceil(jsonArray.length / 3);
-        var rows = 0;
-        var columns = 0;
-        
-        for(rows = 0; rows < numberOfRows; rows++){
-            for(columns=0; columns < 3; columns++){
-                renderThumbNails(rows, columns);
-            }
-        }
-     function renderThumbNails(rows, cols){
-        var position = rows*3 + columns;
-        var templateContent = templateWindow.querySelector(".templatecontent");
-        var imageWrapper = document.createElement("div");
-            imageWrapper.setAttribute("class", "imagewrapper");
-
-        var thumbNailImage = document.createElement("img");
-            thumbNailImage.setAttribute("class", "thumbnailimage");
-            thumbNailImage.src = jsonArray[position].thumbURL;
-        
-            thumbNailImage.addEventListener("click", function(e){
-               changeBackground(position);
-            });    
-        imageWrapper.appendChild(thumbNailImage);
-        templateContent.appendChild(imageWrapper);  
-     }  
-        
-    function changeBackground(position){
-        var url = jsonArray[position].URL;
-        console.log(url);
-        var req = new XMLHttpRequest();
-        req.open( "GET",url, true);
-        req.setRequestHeader('Access-Control-Allow-Origin', '*', "text/html");
-        req.onreadystatechange = function(){
-            if( req.readyState == 4 && req.status == 200){
-                var image = JSON.parse(req.responseText);
-                
-                document.body.style.background="url('image') repeat top right";
-            }
-        };
-    req.send(null);
-    }
-}
+    function changeBackground(itemUrl){
+        var url = itemUrl;
+        document.body.style.backgroundImage = "url("+ url +")";
+     }
 };
